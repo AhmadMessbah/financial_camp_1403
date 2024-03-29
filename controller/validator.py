17d06ca95ac43d40b969b82547f73b87576438ec
Ali <abdb8338@gmail.com>
2024-03-29 19:27:54 +0430

    update 2 validator

--- controller/validator.py
+++ controller/validator.py
@@ -8,3 +8,4 @@ def password_validator(password):
     return bool(re.match("^[A-Za-z0-9]{1}[A-Za-z0-9@]{7,30}$",password))
 def amount_validator(amount):
     return bool(re.match("^[1-9]{1}[0-9]{2,30}$",amount))
+#asdasds
