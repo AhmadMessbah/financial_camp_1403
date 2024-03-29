7253f1344a1a0696db5f76b78a56a5aa90acf04f
Hossein <h.nezafat8@gmail.com>
2024-03-29 18:21:48 +0430

    edited file (1st)

--- test/test.py
+++ test/test.py
@@ -1,4 +1,26 @@
-import controller.person_controller as p_control
+#import controller.person_controller as p_control
+from view.components import *
+
+def login(username , password):
+    pass
+
+
+login_win = Window('Login', 400, 300)
+# person_win = Window('Persons', 400, 300)
+# Tr_win = Window('Transations', 400, 300)
+# Home_win = Window('Home', 400, 300)
+
+
+
+
+# login
+username = TextAndLabel(login_win, "Username", 20, 50)
+password = TextAndLabel(login_win, "Password", 20, 90)
+
+Button(login_win, text="Login", width=10, command=login()).place(x=20, y=400)
+
+
+
 
 # print(p_control.save("mohsen", "akbari"))
 # print(p_control.edit(3,"mohsen", "mohseni"))
@@ -8,3 +30,4 @@ import controller.person_controller as p_control
 
 
 # this is test by rasta banaii
+
