191cbb54bbda597c499b92cc4eab9dc1483c11a9
Ali <abdb8338@gmail.com>
2024-03-29 18:16:09 +0430

    edit validitor

--- controller/validator.py
+++ controller/validator.py
@@ -1,5 +1,8 @@
 import re
-
-
 def name_validator(name):
     return bool(re.match("^[a-zA-Z\s]{3,30}$", name))
+def username_validator(username):
+    return bool(re.match("^[A-Za-z]{1}[A-Za-z0-9]{3,30}$",username))
+
+def password_validator(password):
+    return bool(re.match("^[A-Za-z0-9]{1}[A-Za-z0-9@]{7,30}$",password))
