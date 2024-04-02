9605f3c6ebaf2dedf59483f7ed2d8d8f230dab00
HosseinNezafat <165389481+HosseinNezafat@users.noreply.github.com>
2024-04-02 20:43:23 +0330

    Add files via upload

--- controller/Trans_controller.py
+++ controller/Trans_controller.py
@@ -1,12 +1,41 @@
+from controller.validator import amount_validator
+from model.da.trans_da import TransDa
+
 def tr_save(id, date_time, amount, type):
-    pass
+    try:
+        if amount_validator(amount):
+            da = TransDa()
+            da.save(id, date_time, amount, type)
+            return True, "Saved"
+        else:
+            return False, "Error : Invalid Data"
+    except Exception as e:
+        return False, f"Error : {e}"
 
 def tr_find_all():
-    pass
+    try:
+        da = TransDa()
+        return True, da.find_all()
+    except Exception as e:
+        return False, f"Error : {e}"
 
 def tr_find_by_id(id):
-    pass
+    try:
+        da = TransDa()
+        return True, da.find_by_id(id)
+    except Exception as e:
+        return False, f"Error : {e}"
 
 def tr_balance(id):
-    pass
-
+    balance = 0
+    try:
+        da = TransDa()
+        trs = da.find_by_id(id)
+        for item in trs:
+            if da.type == 0:
+                balance -= da.amount
+            else:
+                balance += da.amount
+        return balance
+    except Exception as e:
+        return False, f"Error : {e}"
